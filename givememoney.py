eae4b122b81d050df0eb66c65bcbd324bc1296ef
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-11-02 17:15:14 +0000

    Create givememoney.py

---
+++ givememoney.py
@@ -0,0 +1,25 @@
+import time
+money = str(input("give me money "))
+if money == "no":
+        print("youre mean")        
+        print ("totallynotnotascammer123 left the chat")
+     
+elif money == "yes":
+        print ("send me  500  pound itune gift  not a scam:]")
+        yes = input (" is it sent ")
+        if yes == "yes":
+                print("btw i am not a scammer  this will go to towards multiple game@charitytings.com☻ dankerschun tchüss")
+   
+elif money == "why":
+        print ("cos funny lol")
+        why = input ("now will you ")
+        if why == "no":
+                print("air strike inbound")
+                x = 5
+                while x > 0:
+                        print(x)
+                        time.sleep(1)
+                        x = x - 1
+                print ("booom!")
+        elif why == "yes":
+                print("thank you for sending the money this will go towards multiple game@charitytings.com☻")
